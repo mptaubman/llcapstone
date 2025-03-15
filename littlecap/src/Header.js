6bc2ae47bfddd0a4e06ce75f73a7791441435c55
Maginolia <maginolia.christensen@gmail.com>
2025-03-15 08:26:39 -0400

    I forgot to save the other files update, it is now saved and ready to add to repository

---
+++ littlecap/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainLogo() {
+    return(
+        <img src="C:\Users\magin\OneDrive\Desktop\Fig Portfolios\Little_lemon_vs\llcap\llcapstone\littlecap\logos\logo.jpg">
+            Little Lemon Head logo
+        </img>
+    );
+}
+
+export default MainLogo;
