9abff57e964633b84ae42aa117112883a4dc8036
Maginolia <maginolia.christensen@gmail.com>
2025-03-18 10:17:03 -0400

    Installed router-dom and updated the navigation coding to reflect links instead of standard a and href tags

--- littlecap/src/App.js
+++ littlecap/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navigation from './components/Nav';
 import Footer from './components/Footer';
+import {Route, Routes} from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <main>Placeholder till i figure out moer</main>
+      <main>Placeholder till i figure out moer </main>
       <Footer />
         Headliner
     </div>
--- littlecap/src/components/Nav.js
+++ littlecap/src/components/Nav.js
@@ -1,29 +1,29 @@
 import React from 'react';
-
+import {Route, Routes} from 'react-router-dom';
 function Navigation(){
     return (
         <nav className='navbar'>
             <img src={require("../assets/logo.jpg")}
             alt="Little Lemon Logo"
             className='nav-logo'></img>
-        <a className="hover-effect" href="index.html">
+        <Link to="/" className="hover-effect">
             <h1>Home</h1>
-        </a>
-        <a className="hover-effect" href="index.html">
+        </Link>
+        <Link to="/About" className="hover-effect">
             <h1>About</h1>
-        </a>
-        <a className='hover-effect' href="index.html">
+        </Link>
+        <Link to="/Menu" className='hover-effect'>
             <h1>Menu</h1>
-        </a>
-      <a className="hover-effect" href="index.html">
+        </Link>
+      <Link to="/Reservations" className="hover-effect">
         <h1>Reservations</h1>
-      </a>
-      <a className="hover-effect" href="index.html">
+      </Link>
+      <Link to="/Order" className="hover-effect">
         <h1>Order</h1>
-      </a>
-      <a className="hover-effect" href="index.html">
+      </Link>
+      <Link to="Login" className="hover-effect">
         <h1>Login</h1>
-      </a>
+      </Link>
         </nav>
     );
 }
