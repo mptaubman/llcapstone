a70c2feabf374209fe715f0ed08810c945ca279a
Maginolia <maginolia.christensen@gmail.com>
2025-03-25 09:23:47 -0400

    Finalized all photos and font styles by adding resources to assets and implemented updated code sources to incorporate.

--- littlecap/src/components/sectionComponents/HomeElements/AboutHero.js
+++ littlecap/src/components/sectionComponents/HomeElements/AboutHero.js
@@ -6,16 +6,16 @@ function AboutHero(){
                 <h2>Chicago</h2>
                 <p className="about-subtext">This is where the text describing the restaurant is going to go.
                     It should have an uplifting story perhaps. Or even focus on the type of food to find at the restaurant.
-                    maybe it would be more beneficial to include any movements or communities they have served from their restaurant.
+                    Maybe it would be more beneficial to include any movements or communities they have served from their restaurant.
                     We could also just keep it simple by stating how long they have been chefs and the specialities in food.
                 </p>
             </section>
             <section className="double-image">
                 <img className="about_1"
-                src={require('../../../assets/bruschetta.jpg')}
+                src={require('../../../assets/Mario_Adrian.jpg')}
                 alt="A dish from the Little Lemon Restaurant."></img>
                 <img className="about_2"
-                src={require('../../../assets/bruschetta.jpg')}
+                src={require('../../../assets/Mario_Adrian_b.jpg')}
                 alt="A dish from the Little Lemon Restaurant."></img>
             </section>
         </article>
--- littlecap/src/components/sectionComponents/HomeElements/HomeHeader.js
+++ littlecap/src/components/sectionComponents/HomeElements/HomeHeader.js
@@ -7,12 +7,14 @@ function HomeHeader(){
                 <section className="hero-text">
                     <h1>Little Lemon</h1>
                     <h3>Chicago</h3>
-                    <p className="subsection">We are a family owned restaurant.</p>
+                    <p className="subsection">We are a family owned restaurant and have been serving the community for over 50 years.
+                        Featuring fresh mediterranean family style dishes, with upbeat modern atmosphere.
+                    </p>
                     <br />
                     <Link className="action-button" to="/BookingPage">Reserve A Table</Link>
                 </section>
                 <section className="hero-img">
-                    <img src={require('../../../assets/bruschetta.jpg')}
+                    <img src={require('../../../assets/hero.jpg')}
                     alt="Little Lemon Restaurant food dish"
                     className="cta_pic"></img>
                 </section>
